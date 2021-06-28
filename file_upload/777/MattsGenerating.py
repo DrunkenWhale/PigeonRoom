c8e53ef05b33a2dc970c77a7e6fa2b43e4662825
Pigeon377 <73925628+Pigeon377@users.noreply.github.com>
2021-06-28 22:46:53 +0800

    Add files via upload

---
+++ file_upload/777/MattsGenerating.py
@@ -0,0 +1,19 @@
+# class main(object):
+#     def __init__(self):
+#         n, r, c = input("n r c").split()
+#         n, r, c = int(n), int(r), int(c)
+#         MattsGenerating(n, r, c)
+#
+#
+# class MattsGenerating():
+#     def __init__(self, n, r, c):
+#         self.n = n
+#         self.r = r
+#         self.c = c
+#         for a in range(c * 6):   # 顶行
+#             print('-', end='')
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
